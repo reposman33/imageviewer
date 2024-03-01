08146fc394cfa18aea1af4d52040435acf64a357
Marc Bakker <webdev@marcbakker.com>
2024-03-01 21:47:22 +0100

    toevoegen method om filenamen op te halen van api

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   selectedFiles?: FileList;
-  api = { fileupload: 'http://localhost:3000/file-upload/' };
+  api = {
+    fileupload: 'http://localhost:3000/file-upload',
+    fileList: 'http://localhost:3000/filelist',
+  };
+  filelistSubject$ = new Subject<String[]>();
+  filelist$ = this.filelistSubject$.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -17,6 +22,12 @@ export class ApiService {
     formData.append('fileName', file.name);
     return this.http.post(this.api.fileupload, formData, {
       responseType: 'text',
+    });
+  }
+
+  getFiles() {
+    this.http.get(this.api.fileList).subscribe((res) => {
+      this.filelistSubject$.next(res as string[]);
     });
   }
 }
